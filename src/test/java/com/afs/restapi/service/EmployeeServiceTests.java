319f60d487f1d6a5714225a9330ae98352aa4228
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-24 18:16:33 +0800

    feat: add implementation and test should_return_created_active_employee_when_create_given_employee_service_and_employee_with_valid_age

--- src/test/java/com/afs/restapi/service/EmployeeServiceTests.java
+++ src/test/java/com/afs/restapi/service/EmployeeServiceTests.java
@@ -57,47 +57,50 @@ class EmployeeServiceTest {
         assertEquals(employee.getGender(), foundEmployee.getGender());
         assertEquals(employee.getSalary(), foundEmployee.getSalary());
     }
-//
-//    @Test
-//    void should_return_employees_by_given_gender_when_get_employees_given_employee_service() {
-//        // Given
-//        Employee alice = new Employee(null, "Alice", 24, "Female", 9000);
-//        List<Employee> employees = List.of(alice);
-//        when(mockedEmployeeRepository.findEmployeeByGender(anyString())).thenReturn(employees);
-//
-//        // When
-//        List<Employee> foundEmployees = employeeService.findByGender("Female");
-//
-//        // Then
-//        assertEquals(employees.size(), foundEmployees.size());
-//        assertEquals(alice.getId(), foundEmployees.get(0).getId());
-//        assertEquals(alice.getName(), foundEmployees.get(0).getName());
-//        assertEquals(alice.getAge(), foundEmployees.get(0).getAge());
-//        assertEquals(alice.getGender(), foundEmployees.get(0).getGender());
-//        assertEquals(alice.getSalary(), foundEmployees.get(0).getSalary());
-//    }
-//
-//    @Test
-//    void should_return_created_active_employee_when_create_given_employee_service_and_employee_with_valid_age() {
-//        // Given
-//        Employee employee = new Employee(null, "Lucy", 20, "Female", 3000);
-//        Employee savedEmployee = new Employee(1L, "Lucy", 20, "Female", 3000);
-//        when(mockedEmployeeRepository.addEmployee(employee)).thenReturn(savedEmployee);
-//
-//        // When
-//        Employee employeeResponse = employeeService.create(employee);
-//
-//        // Then
-//        assertEquals(savedEmployee.getId(), employeeResponse.getId());
-//        assertEquals("Lucy", employeeResponse.getName());
-//        assertEquals(20, employeeResponse.getAge());
-//        assertEquals("Female", employeeResponse.getGender());
-//        assertEquals(3000, employeeResponse.getSalary());
-//        verify(mockedEmployeeRepository).addEmployee(argThat(tempEmployee -> {
-//            assertTrue(tempEmployee.isActive());
-//            return true;
-//        }));
-//    }
+
+    @Test
+    void should_return_employees_by_given_gender_when_get_employees_given_employee_service() {
+        // Given
+        Employee alice = new Employee(null, "Alice", 24, "Female", 9000);
+        List<Employee> employees = List.of(alice);
+        when(employeeJpaRepository.findAllByGender(anyString())).thenReturn(employees);
+
+        // When
+        List<Employee> foundEmployees = employeeService.findAllByGender("Female");
+
+        // Then
+        assertEquals(employees.size(), foundEmployees.size());
+        assertEquals(alice.getId(), foundEmployees.get(0).getId());
+        assertEquals(alice.getName(), foundEmployees.get(0).getName());
+        assertEquals(alice.getAge(), foundEmployees.get(0).getAge());
+        assertEquals(alice.getGender(), foundEmployees.get(0).getGender());
+        assertEquals(alice.getSalary(), foundEmployees.get(0).getSalary());
+    }
+
+    @Test
+    void should_return_created_active_employee_when_create_given_employee_service_and_employee_with_valid_age() {
+        // Given
+        Employee employee = new Employee(null, "Lucy", 20, "Female", 3000);
+        Employee savedEmployee = new Employee(1L, "Lucy", 20, "Female", 3000);
+        when(employeeJpaRepository.save(employee)).thenReturn(savedEmployee);
+
+        // When
+        Employee employeeResponse = employeeService.create(employee);
+
+        // Then
+        assertEquals(savedEmployee.getId(), employeeResponse.getId());
+        assertEquals("Lucy", employeeResponse.getName());
+        assertEquals(20, employeeResponse.getAge());
+        assertEquals("Female", employeeResponse.getGender());
+        assertEquals(3000, employeeResponse.getSalary());
+        verify(employeeJpaRepository).save(argThat(tempEmployee -> {
+            assertEquals("Lucy", tempEmployee.getName());
+            assertEquals(20, tempEmployee.getAge());
+            assertEquals("Female", tempEmployee.getGender());
+            assertEquals(3000, tempEmployee.getSalary());
+            return true;
+        }));
+    }
 //
 //    @Test
 //    void should_throw_exception_when_create_given_employee_service_and_employee_whose_age_is_less_than_18() {
