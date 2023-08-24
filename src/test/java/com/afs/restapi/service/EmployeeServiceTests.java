d6c92573c215cb5da9f879cb73d29030c96e8959
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-24 18:11:49 +0800

    feat: add implementation and test should_return_the_employee_when_get_employee_given_employee_service_and_an_employee_id

--- src/test/java/com/afs/restapi/service/EmployeeServiceTests.java
+++ src/test/java/com/afs/restapi/service/EmployeeServiceTests.java
@@ -40,23 +40,23 @@ class EmployeeServiceTest {
         assertEquals(allEmployees.get(0).getGender(), employee.getGender());
         assertEquals(allEmployees.get(0).getSalary(), employee.getSalary());
     }
-//
-//    @Test
-//    void should_return_the_employee_when_get_employee_given_employee_service_and_an_employee_id() {
-//        // Given
-//        Employee employee = new Employee(1L, "Lucy", 20, "Female", 3000);
-//        when(mockedEmployeeRepository.findEmployeeById(employee.getId())).thenReturn(employee);
-//
-//        // When
-//        Employee foundEmployee = employeeService.findById(employee.getId());
-//
-//        // Then
-//        assertEquals(employee.getId(), foundEmployee.getId());
-//        assertEquals(employee.getName(), foundEmployee.getName());
-//        assertEquals(employee.getAge(), foundEmployee.getAge());
-//        assertEquals(employee.getGender(), foundEmployee.getGender());
-//        assertEquals(employee.getSalary(), foundEmployee.getSalary());
-//    }
+
+    @Test
+    void should_return_the_employee_when_get_employee_given_employee_service_and_an_employee_id() {
+        // Given
+        Employee employee = new Employee(1L, "Lucy", 20, "Female", 3000);
+        when(employeeJpaRepository.findById(employee.getId())).thenReturn(java.util.Optional.of(employee));
+
+        // When
+        Employee foundEmployee = employeeService.findById(employee.getId());
+
+        // Then
+        assertEquals(employee.getId(), foundEmployee.getId());
+        assertEquals(employee.getName(), foundEmployee.getName());
+        assertEquals(employee.getAge(), foundEmployee.getAge());
+        assertEquals(employee.getGender(), foundEmployee.getGender());
+        assertEquals(employee.getSalary(), foundEmployee.getSalary());
+    }
 //
 //    @Test
 //    void should_return_employees_by_given_gender_when_get_employees_given_employee_service() {
