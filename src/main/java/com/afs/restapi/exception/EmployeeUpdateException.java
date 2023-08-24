efef4ac6802efb0b5aa6f4670e6f38b84d16ec06
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-24 21:04:56 +0800

    feat: add Response Status Https Bad Request for Exception Employee Create and Update

--- src/main/java/com/afs/restapi/exception/EmployeeUpdateException.java
+++ src/main/java/com/afs/restapi/exception/EmployeeUpdateException.java
@@ -1,5 +1,9 @@
 package com.afs.restapi.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class EmployeeUpdateException extends RuntimeException {
     public EmployeeUpdateException() {
         super("Employee is inactive");
